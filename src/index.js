1505856cbea441b97dafd8c08387cdb5b851ef22
Devi <devi131780@gmail.com>
2025-01-28 11:37:32 +0530

    created anothter store

--- src/index.js
+++ src/index.js
@@ -1,40 +1,57 @@
-import { createStore } from "redux";
-//action creator
-function orderCake(){
-    return{
-        type:"ORDER_CAKE"
-    }
-}
-
-function updateCakes(){
-    return{
-        type:"UPDATE_CAKE"
-    }
-}
+//action creator 
 //reducer
-const initialState={
-    numOfCakes:10
+//store 
+ 
+import { createStore } from "redux";
+//action creators
+const ORDER_CHOCOLATE="ORDER_CHOCOLATE";
+const UPDATE_CHOCOLATE="UPDATE_CHOCOLATE";
+function orderChocolate(){
+    return {
+        type:ORDER_CHOCOLATE
+    }
 }
-function reducer(state=initialState,action){
-    if(action.type==="ORDER_CAKE"){
-        return {...state,numOfCakes:state.numOfCakes-1}
+function updateChocolate(count){
+    return {
+        type:UPDATE_CHOCOLATE,
+        playload:count
     }
-    else if(action.type==="UPDATE_CAKE"){
-        return {...state,numOfCakes:state.numOfCakes+1}
-    }
-    else{
-        return state;
-    }
-    
 }
-//store
-const store=createStore(reducer);
-console.log("initialState:", store.getState());
-const unsubscribe=store.subscribe(()=>{
-    console.log("updatedState:",store.getState());
-})
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(updateCakes());
-store.dispatch(updateCakes());
 
+const initialState={
+    numOfChocolates:10
+}
+//reducer =it is a pure javascript function 
+
+function reducer(state=initialState,action){
+    switch(action.type){
+        case "ORDER_CHOCOLATE":
+            return {
+                ...state,
+                numOfChocolates:state.numOfChocolates-1
+            }
+        case "UPDATE_CHOCOLATE":
+            return {
+                ...state,
+                numOfChocolates:state.numOfChocolates+5
+            }
+        default:
+            return state    
+    }
+}
+
+const store=createStore(reducer);
+
+console.log("initialState:",store.getState());
+
+const unsubscribe= store.subscribe(()=>{
+    console.log("updatedState:",store.getState())
+})
+
+store.dispatch(orderChocolate());
+store.dispatch(orderChocolate());
+store.dispatch(orderChocolate());
+store.dispatch(orderChocolate());
+store.dispatch(updateChocolate());
+unsubscribe();
+store.dispatch(updateChocolate());
